7d8dbd069ac046a8905af91162df642c67eb5bec
KareemYoussry <kareem.youssry567@gmail.com>
2022-05-11 15:29:44 +0200

    New LCD Driver
    The LCD driver before was not intended for the LCD display we have.

--- LCD/LCD.c
+++ LCD/LCD.c
@@ -6,12 +6,13 @@ void LCD_Write_Hb(unsigned char data, unsigned char control)
 	data		&=	0xF0;	/* Extract upper nibble for data */
 	control	&=	0x0F;	/* Extract lower nibble for control */
 	GPIO_PORTB_DATA_R = data | control;	/* Set RS and R/W bits to zero for write operation */
-	GPIO_PORTB_DATA_R = data | control | EN;	/* Provide Pulse to Enable pin to perform wite operation */
-	
-	Systick_Wait_1us();
+	GPIO_PORTB_DATA_R = data | control | 0x20;	/* Provide Pulse to Enable pin to perform wite operation */
+	Systick_Wait_1ms();
+	GPIO_PORTB_DATA_R &= ~0x20;
 	
 	GPIO_PORTB_DATA_R = data;	/* Send data */
 	GPIO_PORTB_DATA_R = 0;		/* Stop writing data to LCD */
+	Systick_Wait_ms(5);
 }
 
 void LCD_Cmd(unsigned char command)
@@ -20,9 +21,9 @@ void LCD_Cmd(unsigned char command)
 	LCD_Write_Hb(command << 4, 0);     /* Write lower nibble to LCD */
 	
 	if (command < 4)
-		Systick_Wait_ms(2);         /* 2ms delay for commands 1 and 2 */
+		Systick_Wait_ms(2);		/* 2ms delay for commands 1 and 2 */
 	else
-		Systick_Wait_us(40);        /* 40us delay for other commands */
+		Systick_Wait_us(40);	/* 40us delay for other commands */
 }
 
 void LCD_Init(void)
@@ -31,12 +32,11 @@ void LCD_Init(void)
 	GPIO_PORTB_DIR_R	|=	0xFF;		/* Set GPIOB all pins a digital output pins */
 	GPIO_PORTB_DEN_R	|=	0xFF;		/* Declare GPIOB pins as digital pins */
 	
-	// The following commands are defined in the header file
-	LCD_Cmd(Set5x7FontSize);		/* select 5x7 font size and 2 rows of LCD */
+	// The following commands are defined in the header file	
 	LCD_Cmd(Function_set_4bit);	/* Select 4-bit Mode of LCD */
+	LCD_Cmd(displayOn);					/* Sets the display on */
 	LCD_Cmd(moveCursorRight);		/* shift cursor right */
 	LCD_Cmd(clear_display);			/* clear whatever is written on display */
-	LCD_Cmd(cursorBlink);				/* Enable Display and cursor blinking */
 }
 
 void LCD_Write_Char(unsigned char data)
--- LCD/LCD.h
+++ LCD/LCD.h
@@ -12,18 +12,17 @@
 /* Define useful symbolic names for LCD commands */
 #define clear_display     0x01
 #define returnHome        0x02
-#define moveCursorRight   0x06
-#define moveCursorLeft    0x08
-#define shiftDisplayRight 0x1C
+#define Entry_mode        0x06
+#define	displayOn					0x0F
+#define moveCursorRight   0x14
+#define moveCursorLeft    0x10
+#define shiftDisplayRight 0x1F
 #define shiftDisplayLeft  0x18
-#define cursorBlink       0x0F
 #define cursorOff         0x0C
 #define cursorOn          0x0E
 #define Function_set_4bit 0x28
-#define Function_set_8bit 0x38
-#define Entry_mode        0x06
-#define Function_8_bit    0x32
-#define Set5x7FontSize    0x20
+#define Function_set_8bit 0x33
+#define Set5x8FontSize    0x20
 #define FirstRow          0x80
 
 /* LCD Function Prototypes */
