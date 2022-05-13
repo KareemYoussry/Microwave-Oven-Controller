4b58c160182d16a3bdfcf80f116948f64c7f4135
GeorgeEFH <goemadgo@gmail.com>
2022-05-14 01:33:25 +0200

    D key on keypad (1)
    Made a function which:
    1- check if the "D key is pressed
    2- shows "Cooking Time?"
    3- check if any value is entered for cooking time
    
    some other functionalities  are required, such as:
    1- check if a key is pressed during entering cooking time
    2- waiting time
    
    Also, the code isn't tested yet.

--- main.c
+++ main.c
@@ -1,7 +1,66 @@
 #include "tm4c123gh6pm.h"
 #include "./LCD/LCD.h"
 #include "./keypad/keypad.h"
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
 
+void checkswitch () {
+	
+}
+void checknum(int *value1,int *value2,int *value3,int *value4, int n){
+	int i;
+	char word[5];
+	for (i=0;i<10;i++){
+		if (*value1 == i){
+			switch (n){
+				case 1:
+					strcpy(word, "00:00");
+					LCD_Cmd(clear_display);
+					word[4] = *value1 + '0';
+					LCD_String(word);
+					break;
+				
+				case 2:
+					strcpy(word, "00:00");
+					LCD_Cmd(clear_display);
+					word[4] = *value2 + '0';
+					word[3] = *value1 + '0';
+					LCD_String(word);
+					break;
+				case 3:
+					strcpy(word, "00:00");
+					LCD_Cmd(clear_display);
+					word[4] = *value3 + '0';
+					word[3] = *value2 + '0';
+					word[1] = *value1 + '0';
+					LCD_String(word);
+					break;
+				case 4:
+					strcpy(word, "00:00");
+					LCD_Cmd(clear_display);
+					word[4] = *value4 + '0';
+					word[3] = *value3 + '0';
+					word[1] = *value2 + '0';
+					word[0] = *value1 + '0';
+					LCD_String(word);
+					break;
+		}
+	}
+}
+	}
+
+void D_Key (const char D){
+		int value1 = 0,value2 = 0,value3 = 0,value4 = 0,i;
+		if (!strcmp(D, "D")){
+			return;
+		}
+		LCD_String("Cooking Time?");
+		value1 = keypad_getkey() - '0';
+		checknum(&value1,&value2,&value3,&value4,1);
+		
+		
+}
 
 int main(void)
 {
@@ -18,12 +77,8 @@ int main(void)
 	
 	while (1)
 	{
-		string = keypad_getkey();
-		LCD_Write_Char(string);
-		
+		D_Key (keypad_getkey());
 		Systick_Wait_ms(250);
-		
-
 		string = 0;
 	}
 
