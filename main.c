4e31d38b360f0b0d599a3de8082490b095a0ad3e
GeorgeEFH <goemadgo@gmail.com>
2022-05-16 00:18:11 +0200

    Almost finished
    The 4 errors were tackled successfully, the program should run smoothly on TIVA
    Pending run on TIVA

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #include "./Microwave_Functions/Microwave_Functions.h"
 
 
-void checknum(int values [4], int n){
+void checknum(unsigned char values [4], int n){
 	char word[5];
 	strcpy(word, "00:00"); 
 		if (!((isdigit (values[0]))  // checking if values entered are a number or not
@@ -19,48 +19,56 @@ void checknum(int values [4], int n){
 
 			switch (n){
 				case 0: // first case: first digit is entered
-					word[4] = values[0] + '0';
+					word[4] = values[0];
 					LCD_StringPos(word,2,0);
 					break;	
 				case 1: // Second case: second digit is entered
-					word[4] = values[1] + '0';
-					word[3] = values[0] + '0';
+					word[4] = values[1];
+					word[3] = values[0];
 					LCD_StringPos(word,2,0);
 					break;
 				case 2: // Third case: third digit is entered
-					word[4] = values[2] + '0';
-					word[3] = values[1] + '0';
-					word[1] = values[0] + '0';
+					word[4] = values[2];
+					word[3] = values[1];
+					word[1] = values[0];
 					LCD_StringPos(word,2,0);
 					break;
 				case 3: // Fourth case: fourth digit is entered
 					if (values[0] >=3 && values[1] > 0)
 						LCD_StringPos("Error",2,0);  // checking if the minutes are less than or equal 30 minutes
 					
-					word[4] = values[3] + '0';
-					word[3] = values[2] + '0';
-					word[1] = values[1] + '0';
-					word[0] = values[0] + '0';
+					word[4] = values[3];
+					word[3] = values[2];
+					word[1] = values[1];
+					word[0] = values[0];
 					LCD_StringPos(word,2,0);
 					break;
 		}
 	}
 
 void D_Key (char D){
-			int values[5],ite; // declaring array to use as time
+			unsigned char values[4];
+			int ite; // declaring array to use as time
 //		if (!strcmp(D , "D")){  // checking if the key entered is D
 //			return;
 //		}
 		LCD_StringPos("Cooking Time?", 1, 0); // Displaying Cooking Time on LCD
 		for (ite = 0 ; ite <4 ; ite++){  // Iterating to get values and print them on LCD
-			values[ite] = keypad_getkey() - '0'; // Get value
-			checknum(&values[ite],ite);
+			do{
+				values[ite] = keypad_getkey(); // Get value
+			}
+			while (values[ite] >= '0' && values[ite] <= '9');
+				checknum(&values[ite],ite);
 		}
 		if ((GPIO_PORTF_DATA_R&0x01) != 0x01) 
 			LCD_Cmd(clear_display);
 		if ((GPIO_PORTF_DATA_R&0x10) != 0x10) {
-			unsigned char secs [2] = {values[2] + '0', values[3] + '0'}; // declaring array for seconds
-			unsigned char mins [2] = {values[0] + '0', values[1] + '0'}; // declaring array for minutes
+			unsigned char secs [2]; // declaring array for seconds
+			unsigned char mins [2]; // declaring array for minutes
+			secs [0] = values[2];
+			secs [1] = values[3];
+			mins [0] = values[0];
+			mins [1] = values[1];
 			LCD_CountDown (secs,mins);
 	}
 }
