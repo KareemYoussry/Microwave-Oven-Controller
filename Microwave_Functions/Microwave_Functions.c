e2ea34d2b4dce41941648149475a83dd289fb8be
ahmedmaher198 <105322043+ahmedmaher198@users.noreply.github.com>
2022-05-15 19:47:47 +0200

    beef function

--- Microwave_Functions/Microwave_Functions.c
+++ Microwave_Functions/Microwave_Functions.c
@@ -28,18 +28,48 @@ void chiken(void){
 			break;  
 		else { 
 			LCD_StringPos("Err", 2, 0);
-			Systick_Wait_ms(2000);
+			Systick_Wait_ms(2000); 
+		}
+		
 	}while(1);
 		input -= 48 ;
 	int time;
 	time = input*12; 
 	unsigned char mins[2]={0};
 	unsigned char secs[2]={0}; 
-	itoa(time/60,mins,10);
-	itoa(time%60,secs,10);
+	mins[0]=time/600 ; // calculate tens of minutes
+	mins[1]=(time/60)%10 ; // calculate units of minutes
+	secs[0]=(time%60)/10; // calculate tens of seconds 
+	secs[1]=(time%60)%10 ; // calculate units of seconds 
 	LCD_CountDown(mins,secs);
 	
 }
+
+void Beef(void){
+	char input=0;
+	LCD_StringPos("Beef weight!",1,0); 
+	
+	do {
+	input =keypad_getkey(); 
+		if (input>'0'&&input<='9')
+			break;  
+		else { 
+			LCD_StringPos("Err", 2, 0);
+			Systick_Wait_ms(2000); 
+		}
+		
+	}while(1);
+		input -= 48 ;
+	int time;
+	time = input*30; 
+	unsigned char mins[2]={0};
+	unsigned char secs[2]={0}; 
+	mins[0]=time/600 ; // calculate tens of minutes
+	mins[1]=(time/60)%10 ; // calculate units of minutes
+	secs[0]=(time%60)/10; // calculate tens of seconds 
+	secs[1]=(time%60)%10 ; // calculate units of seconds 
+	LCD_CountDown(mins,secs);
+
 
 //Counter in minutes and seconds (making the count down appear in the LCD)
 //sec[]: 2d-array:{tens of seconds, ones of seconds} as this {5,9} = 59 second
