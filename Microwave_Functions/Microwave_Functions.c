b34ca3bf5849084c1f0d76362b287862505528a9
Abdo865 <abdonasser865@gmail.com>
2022-05-23 02:09:58 +0200

    Adding The D-Key
    Co-Authored-By: ahmedmaher198 <105322043+ahmedmaher198@users.noreply.github.com>

--- Microwave_Functions/Microwave_Functions.c
+++ Microwave_Functions/Microwave_Functions.c
@@ -9,27 +9,11 @@ unsigned char button_in2;
 unsigned char button_in1;
 extern volatile unsigned char Dflag; 
 
-void popCorn(void){
-	unsigned char mins[2] = {0, 0},sec[2]={5,0};
-	
-	LCD_Cmd(clear_display);
-	LCD_StringPos("Popcorn", 1, 0);
-	do{
-		button_in2 = sw2_input();
-  }while(button_in2);
-	flag = 0;
-	LCD_StringPos("Time: ", 2, 0);
-	leds_on();
-
-	LCD_CountDown(sec,mins);
-	
-	LCD_Cmd(clear_display);
-}
-
 
 //Counter in minutes and seconds (making the count down appear in the LCD)
 //sec[]: 2d-array:{tens of seconds, ones of seconds} as this {5,9} = 59 second
 //min[]: 2d-array:{tens of minutes, ones of minutes} as this {1,5} = 15 min
+// send the numbers as integers not ascii
 
 
 void LCD_CountDown(unsigned char sec[],unsigned char min[])
@@ -67,35 +51,89 @@ void LCD_CountDown(unsigned char sec[],unsigned char min[])
 	}
 }
 
-char checknum(unsigned char values [], int n){
-	char word[5] = "00:00";
-			switch (n){
-				case 0: // first case: first digit is entered
-					word[4] = values[0];
-					LCD_StringPos(word,2,0);
-					break;	
-				case 1: // Second case: second digit is entered
-					word[4] = values[1];
-					word[3] = values[0];
-					LCD_StringPos(word,2,0);
-					break;
-				case 2: // Third case: third digit is entered
-					word[4] = values[2];
-					word[3] = values[1];
-					word[1] = values[0];
-					LCD_StringPos(word,2,0);
-					break;
-				case 3: // Fourth case: fourth digit is entered
-					if ((values[0] >='3') && (values[1] > '0')&&(values[2] > '0')&&(values[3] > '0'))
-						return 1;
-					word[4] = values[3];
-					word[3] = values[2];
-					word[1] = values[1];
-					word[0] = values[0];
-					LCD_StringPos(word,2,0);
-					break;
+
+void popCorn(void){
+	unsigned char mins[2] = {0, 0},sec[2]={5,0};
+	
+	LCD_Cmd(clear_display);
+	LCD_StringPos("Popcorn", 1, 0);
+	do{
+		button_in2 = sw2_input();
+  }while(button_in2);
+	flag = 0;
+	LCD_StringPos("Time: ", 2, 0);
+	leds_on();
+
+	LCD_CountDown(sec,mins);
+	
+	LCD_Cmd(clear_display);
+}
+
+void Beef(void){
+	unsigned char secs [2],mins [2]; // declaring array for seconds and minutes;
+	unsigned char weight = 0;
+	int time = 0;
+	unsigned char input;
+	LCD_StringPos("Beef weight?",1,0); 
+	while(1){
+		input = keypad_getkey(); 
+		if (input>'0'&&input<='9'){
+			input = keypad_getkey(); 
+			Systick_Wait_ms(250);
+			break; 
 		}
-			return 0;
+		else {
+			LCD_StringPos("Err", 2, 0);
+			Systick_Wait_ms(2000); 
+			LCD_Cmd(clear_display);
+		}
+	}
+	
+	LCD_Cmd(SecondRow);
+	LCD_Write_Char(input);
+	Systick_Wait_ms(2000);
+	flag = 0;
+	time = 30 * (input - '0');
+	mins[0] = 0;
+	mins[1] = time /60;
+	secs[0] = (time%60) / 10 ;
+	secs[1] = time - mins[1] * 60 - secs[0] * 10;
+	
+	LCD_CountDown (secs,mins);
+
+}
+
+void Chicken(void){
+	unsigned char input=0;
+	unsigned char mins[2];
+	unsigned char secs[2];
+	int time;
+	
+	LCD_StringPos("Chicken weight?",1,0); 
+	
+	do {
+	input =keypad_getkey(); 
+		if (input>'0'&&input<='9')
+			break;
+		else { 
+			LCD_StringPos("Err", 2, 0);
+			Systick_Wait_ms(2000); 
+		}
+	}while(1);
+	LCD_Cmd(SecondRow);
+	LCD_Write_Char(input);
+	Systick_Wait_ms(2000);
+	flag = 0;
+	
+	input -= 48 ;
+	time = input*12; 
+
+	
+	mins[0]	=	0;							// calculate tens of minutes
+	mins[1]	=	(time/60)%10 ;	// calculate units of minutes
+	secs[0]	=	(time%60)/10;		// calculate tens of seconds 
+	secs[1]	=	(time%60)%10 ;	// calculate units of seconds 
+	LCD_CountDown(secs,mins);
 }
 
 void D_Key (void){
@@ -129,4 +167,35 @@ void D_Key (void){
 
 	Dflag = 0;
 	LCD_CountDown (secs,mins);
-}	
+}
+
+char checknum(unsigned char values [], int n){
+	char word[5] = "00:00";
+			switch (n){
+				case 0: // first case: first digit is entered
+					word[4] = values[0];
+					LCD_StringPos(word,2,0);
+					break;	
+				case 1: // Second case: second digit is entered
+					word[4] = values[1];
+					word[3] = values[0];
+					LCD_StringPos(word,2,0);
+					break;
+				case 2: // Third case: third digit is entered
+					word[4] = values[2];
+					word[3] = values[1];
+					word[1] = values[0];
+					LCD_StringPos(word,2,0);
+					break;
+				case 3: // Fourth case: fourth digit is entered
+					if ((values[0] >='3') && (values[1] > '0')&&(values[2] > '0')&&(values[3] > '0'))
+						return 1;
+					word[4] = values[3];
+					word[3] = values[2];
+					word[1] = values[1];
+					word[0] = values[0];
+					LCD_StringPos(word,2,0);
+					break;
+		}
+			return 0;
+}
--- Microwave_Functions/Microwave_Functions.h
+++ Microwave_Functions/Microwave_Functions.h
@@ -1,8 +1,14 @@
 #ifndef __MICROWAVE_FUNCTIONS_H__
 #define __MICROWAVE_FUNCTIONS_H__
 
-void popCorn(void);
+
 void LCD_CountDown(unsigned char sec[],unsigned char min[]);
+void popCorn(void);
+void Beef(void);
+void Chicken(void);
+void D_Key(void);
+char checknum(unsigned char values [], int n);
+
 
 
 #endif
